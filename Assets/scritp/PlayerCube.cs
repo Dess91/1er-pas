e2d1b21638f0843f6baaa52555a0241d62397c32
louison <louison.gilles@supdecreation.com>
2021-05-17 14:24:06 +0200

    Camero no degeu

--- Assets/scritp/PlayerCube.cs
+++ Assets/scritp/PlayerCube.cs
@@ -12,7 +12,21 @@ public class PlayerCube : MonoBehaviour
     void UpdateControlChild() {
         transform.Find("controle").gameObject.SetActive(isControlled);
     }
-void Move() {
+
+    Vector3 TransformVectorByCameraRotationY(Vector3 vector) {
+        // Ouais, ces 3 lignes sont compliquées :
+        // Il s'agit d'appliquer la rotation de la camera (sur l'axe Y uniquement)
+        // au mouvement que l'on souhaite affecter au "rigid body". Sans cette 
+        // opération, le mouvement s'exprime en "absolu" selon le repère "monde". 
+        // Grâce au quaternion le mouvement (x, y, z) est exprimé selon le repère 
+        // de la caméra : ainsi flèche gauche désigne un mouvement latéral vers la 
+        // gauche de la caméra (et non vers la gauche du repère "monde").
+        float cameraRotationY = Camera.main.transform.rotation.eulerAngles.y;
+        Quaternion cameraQuaternion = Quaternion.Euler(0, cameraRotationY, 0);
+        return cameraQuaternion * vector;
+    }
+
+    void Move() {
         float x = speed * Input.GetAxis("Horizontal");
         float y = body.velocity.y;
         float z = speed * Input.GetAxis("Vertical");
@@ -21,7 +35,9 @@ void Move() {
             y = jumpSpeed;
         }
 
-        body.velocity = new Vector3(x, y, z);
+        Vector3 velocity = new Vector3(x, y, z);
+        velocity = TransformVectorByCameraRotationY (velocity);
+        body.velocity = velocity;
     }
 
     void Start() {
